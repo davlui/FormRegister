29659d87049eafa116356b8473fa285adc2f338c
David Luis <david.luis@informito.pt>
2022-03-06 18:59:53 +0000

    Install and Config EF Core and First Migration

---
+++ FormRegisterWeb/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FormRegisterWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormRegisterWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ FormRegisterWeb/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace FormRegisterWeb.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; } 
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
