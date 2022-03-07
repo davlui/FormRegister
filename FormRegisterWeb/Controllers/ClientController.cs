45ecf4173f11f1cd89f75177f2fcb13341909cd3
David Luis <david.luis@informito.pt>
2022-03-07 23:17:42 +0000

    Edit client

--- FormRegisterWeb/Controllers/ClientController.cs
+++ FormRegisterWeb/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
-﻿using FormRegisterWeb.Repositories.Interfaces;
+﻿using FormRegisterWeb.Models;
+using FormRegisterWeb.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FormRegisterWeb.Controllers
@@ -12,6 +13,7 @@ namespace FormRegisterWeb.Controllers
             _repository = repository;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var clients = _repository.GetClients();
@@ -19,11 +21,48 @@ namespace FormRegisterWeb.Controllers
             return View(clients);
         }
 
+        [HttpGet]
         public IActionResult Detail(int id)
         {
             var client = _repository.GetClientById(id);
 
             return View(client);
         }
+
+
+        // GET
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            var client = _repository.GetClientById(id.Value);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Client obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.UpdateClient(obj);
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
     }
 }
