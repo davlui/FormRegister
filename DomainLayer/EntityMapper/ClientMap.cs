c82f18b23e48beb573adb21bc46feb6d3d563e33
David Luis <david.luis@informito.pt>
2022-03-12 19:37:54 +0000

    Api - Domain Models

---
+++ DomainLayer/EntityMapper/ClientMap.cs
@@ -0,0 +1,27 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DomainLayer.EntityMapper
+{
+    public class ClientMap : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.HasKey(x => x.Id).HasName("pk_clientid");
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("Id")
+                .HasColumnType("INT");
+
+            builder.Property(x => x.CreatedDate)
+                .HasColumnName("created_date")
+                .HasColumnType("datetime");
+
+            builder.Property(x => x.ModifiedDate)
+                .HasColumnName("modified_date")
+                .HasColumnType("datetime");
+
+        }
+    }
+}
